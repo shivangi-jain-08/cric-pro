6185654c701336fd1b2eebe50202b913cc0291db
shivangi-jain-08 <shivangi.jain@kalvium.community>
2023-10-02 04:23:32 +0530

    final

---
+++ app.js
@@ -0,0 +1,132 @@
+// Sehwag image comes up on click
+// declaring variables
+    var sehwag = document.getElementById("sehwag");
+    var sehwagImg = document.getElementById("sehwag-img");
+// adding event listener
+    sehwag.addEventListener("click",function(){
+    sehwagImg.style.display = "block";
+    sehwagImg.onmouseleave=()=>{
+        sehwagImg.style.display="none";
+    }
+});
+
+// Sachin image comes up on click
+// declaring variables
+    var sachin = document.getElementById("sachin");
+    var sachinImg = document.getElementById("sachin-img");
+// adding event listener
+    sachin.addEventListener("click",function(){
+    sachinImg.style.display = "block";
+    sachinImg.onmouseleave=()=>{
+        sachinImg.style.display="none";
+    }
+});
+
+// gambhir image comes up on click
+// declaring variables
+    var gambhir = document.getElementById("gambhir");
+    var gambhirImg = document.getElementById("gambhir-img");
+// adding event listener
+    gambhir.addEventListener("click",function(){
+    gambhirImg.style.display = "block";
+    gambhirImg.onmouseleave=()=>{
+        gambhirImg.style.display="none";
+    }
+    
+});
+
+// kohli image comes up on click
+// declaring variables
+    var kohli = document.getElementById("kohli");
+    var kohliImg = document.getElementById("kohli-img");
+// adding event listener
+    kohli.addEventListener("click",function(){
+    kohliImg.style.display = "block";
+    kohliImg.onmouseleave=()=>{
+        kohliImg.style.display="none";
+    }
+});
+
+// dhoni image comes up on click
+// declaring variables
+    var dhoni = document.getElementById("dhoni");
+    var dhoniImg = document.getElementById("dhoni-img");
+// adding event listener
+    dhoni.addEventListener("click",function(){
+    dhoniImg.style.display = "block";
+    dhoniImg.onmouseleave=()=>{
+        dhoniImg.style.display="none";
+    }
+});
+
+// yuvraj image comes up on click
+// declaring variables
+    var yuvraj = document.getElementById("yuvraj");
+    var yuvrajImg = document.getElementById("yuvraj-img");
+// adding event listener
+    yuvraj.addEventListener("click",function(){
+    yuvrajImg.style.display = "block";
+    yuvrajImg.onmouseleave=()=>{
+        yuvrajImg.style.display="none";
+    }
+});
+
+// raina image comes up on click
+// declaring variables
+    var raina = document.getElementById("raina");
+    var rainaImg = document.getElementById("raina-img");
+// adding event listener
+    raina.addEventListener("click",function(){
+    rainaImg.style.display = "block";
+    rainaImg.onmouseleave=()=>{
+        rainaImg.style.display="none";
+    }
+});
+
+// harbhajan image comes up on click
+// declaring variables
+    var harbhajan = document.getElementById("harbhajan");
+    var harbhajanImg = document.getElementById("harbhajan-img");
+// adding event listener
+    harbhajan.addEventListener("click",function(){
+    harbhajanImg.style.display = "block";
+    harbhajanImg.onmouseleave=()=>{
+        harbhajanImg.style.display="none";
+    }
+});
+
+// zaheer image comes up on click
+// declaring variables
+    var zaheer = document.getElementById("zaheer");
+    var zaheerImg = document.getElementById("zaheer-img");
+// adding event listener
+    zaheer.addEventListener("click",function(){
+    zaheerImg.style.display = "block";
+    zaheerImg.onmouseleave=()=>{
+        zaheerImg.style.display="none";
+    }
+});
+
+// munaf image comes up on click
+// declaring variables
+    var munaf = document.getElementById("munaf");
+    var munafImg = document.getElementById("munaf-img");
+// adding event listener
+    munaf.addEventListener("click",function(){
+    munafImg.style.display = "block";
+    munafImg.onmouseleave=()=>{
+        munafImg.style.display="none";
+    }
+});
+
+// sreesanth image comes up on click
+// declaring variables
+    var sreesanth = document.getElementById("sreesanth");
+    var sreesanthImg = document.getElementById("sreesanth-img");
+// adding event listener
+    sreesanth.addEventListener("click",function(){
+    sreesanthImg.style.display = "block";
+    sreesanthImg.onmouseleave=()=>{
+        sreesanthImg.style.display="none";
+    }
+});
